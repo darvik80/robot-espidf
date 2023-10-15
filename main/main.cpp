72840d6a4d1699b54bc61d265e757037938eb911
Ivan Kishchenko <darvik@crearts.xyz>
2023-10-15 16:30:31 +0700

    support joystick PG-9157

--- main/main.cpp
+++ main/main.cpp
@@ -1,10 +1,35 @@
-#include <stdio.h>
-#include <core/Application.h>
+#include <core/Core.h>
+#include <core/system/storage/NvsStorage.h>
+#include <core/system/telemetry/TelemetryService.h>
+#include <core/system/wifi/WifiService.h>
+#include <core/system/mqtt/MqttService.h>
+#include <core/system/console/Console.h>
+#include <bluetooth/BTManager.h>
+#include <bluetooth/BleDiscovery.h>
+#include <bluetooth/BTHidScanner.h>
 
-class Robot : public Application {
+class Robot : public Application<Robot> {
+public:
+protected:
+    void userSetup() override {
+        getRegistry().create<NvsStorage>();
+        getRegistry().create<TelemetryService>();
+        getRegistry().create<WifiService>();
+        auto &mqtt = getRegistry().create<MqttService>();
+        mqtt.addJsonProcessor<Telemetry>("/telemetry");
 
+        getRegistry().create<UartConsoleService>();
+        getRegistry().create<BTManager>();
+        getRegistry().create<BleDiscovery>();
+        getRegistry().create<BTHidScanner>();
+    }
 };
 
 extern "C" void app_main() {
+    auto app = std::make_shared<Robot>();
+    app->setup();
 
+    app->process();
+
+    app->destroy();
 }
